ae83f61cd669af9e13702acde3aaf4059319d329
TanakaMasunda <masundatanaka@gmail.com>
2022-09-21 17:18:31 -0700

    config management

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# Create a file using puppet with 4 parameters
+file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
